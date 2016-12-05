3ab02fa4f8e0f56977e29005cd6593371c3a462e
Odinaka Amadi <naka506@yahoo.com>
2016-12-05 00:27:14 -0600

    Add files via upload

--- entertainment_center.py
+++ entertainment_center.py
@@ -8,21 +8,21 @@ toy_story = media.Movie("Toy Story",
 # This file stores the movie information. (Title, Image URL, Youtube URL)
 avatar = media.Movie("Avatar",
                      "A marine on an alien planet",
-                     "https://en.wikipedia.org/wiki/File:Avatar-Teaser-Poster.jpg",
+                     "https://upload.wikimedia.org/wikipedia/en/thumb/b/b0/Avatar-Teaser-Poster.jpg/220px-Avatar-Teaser-Poster.jpg",
                      "https://www.youtube.com/watch?v=5PSNL1qE6VY")
 
 the_departed = media.Movie("The Departed",
                "A rat and a mole",
-               "https://en.wikipedia.org/wiki/File:Departed234.jpg",
+               "https://upload.wikimedia.org/wikipedia/en/thumb/5/50/Departed234.jpg/220px-Departed234.jpg",
                "https://www.youtube.com/watch?v=iQpb1LoeVUc")
 
 school_of_rock = media.Movie("School of rock",
                "a teacher starting a rock band",
-               "https://en.wikipedia.org/wiki/File:School_of_Rock_Poster.jpg",
+               "https://upload.wikimedia.org/wikipedia/en/thumb/1/11/School_of_Rock_Poster.jpg/220px-School_of_Rock_Poster.jpg",
                "https://www.youtube.com/watch?v=3PsUJFEBC74")
 love_and_basketball = media.Movie("love_and_basketball",
                       "a boy and girl fall in love and play basketball",
-                      "https://en.wikipedia.org/wiki/File:LBmoviePoster.jpg",
+                      "https://upload.wikimedia.org/wikipedia/en/0/02/LBmoviePoster.jpg",
                       "https://www.youtube.com/watch?v=Ur83i6_BjbE")
                                     
 movies = [toy_story,avatar,school_of_rock,love_and_basketball]
